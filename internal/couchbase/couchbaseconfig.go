d2c9734e93c6f0ae755086036504a596f2422598
ben wornom <bworn621@students.bju.edu>
2018-06-20 18:31:31 -0400

    cbconfig changes

--- internal/couchbase/couchbaseconfig.go
+++ internal/couchbase/couchbaseconfig.go
@@ -1,3 +1,4 @@
+// Couchbase Connector is used for connecting to and working with a couchbase bucket.
 package couchbase
 
 import (
@@ -8,32 +9,26 @@ import (
 	"github.com/couchbase/gocb"
 )
 
+// Couchbase is a couchbase bucket connection.
+// Bucket is the bucket listed in the connection string.
+// Doc is the document structure a client document.
 type Couchbase struct {
-	Bucket *gocb.Bucket
-	Doc    *Doc
+	Bucket     *gocb.Bucket
+	Doc        *Doc
+	bucketName string
 }
 
+// Doc is the document that is associated with the client.
+// ID is the client's ID.
+// Events is the list of client eventID's
 type Doc struct {
 	ID     string   `json:"ID,omitempty"`
 	Events []string `json:"Events"`
 }
 
-func (c *Couchbase) collectEvents(clientID string) error {
-	var err error
-	var docFrag *gocb.DocumentFragment
-	docFrag, err = c.Bucket.LookupIn(fmt.Sprintf("doppler:client:%s", clientID)).Get("Events").Execute()
-	if err != nil {
-		return err
-	}
-	// get the Events array into a slice
-	docFrag.Content("Events", &c.Doc.Events) // Error, but why is Doc.Events null?
-	if err != nil {
-
-		return err
-	}
-	return nil
-}
-
+// ClientExists determines whether or not a couchbase client exists or not.
+// clientID is the client's ID.
+// returns true if the document exists and false otherwise.
 func (c *Couchbase) ClientExists(clientID string) bool {
 	err := c.collectEvents(clientID)
 	if err != nil {
@@ -46,6 +41,51 @@ func (c *Couchbase) ClientExists(clientID string) bool {
 	return true
 }
 
+// collectEvents gets the list of eventID's for the client from the couchbase document.
+// clientID is the client's ID.
+func (c *Couchbase) collectEvents(clientID string) error {
+	var err error
+	var docFrag *gocb.DocumentFragment
+	docFrag, err = c.Bucket.LookupIn(fmt.Sprintf("%s:client:%s", c.bucketName, clientID)).Get("Events").Execute()
+	if err != nil {
+		return err
+	}
+	// get the Events array into a slice
+	docFrag.Content("Events", &c.Doc.Events) // Error, but why is Doc.Events null?
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// EventEnsure adds the provided event to the client's document if it is not already there.
+// clientID is the client's ID.
+// eventID is the client's event ID.
+func (c *Couchbase) EventEnsure(clientID, eventID string) {
+	_, err := c.Bucket.MutateIn(fmt.Sprintf("%s:client:%s", c.bucketName, clientID), 0, 0).ArrayAddUnique("Events", eventID, false).Execute()
+	if err != nil {
+		if err.Error() != "subdocument mutation 0 failed (given path already exists in the document)" {
+			panic(err)
+		}
+	}
+}
+
+// CreateDocument creates a document for the client in the couchbase bucket.
+// clientID is the client's ID.
+// eventID is the client's event ID.
+// Note: this resets the Doc data of the connector.
+func (c *Couchbase) CreateDocument(clientID, eventID string) {
+	c.Doc.Events = append(c.Doc.Events, eventID)
+	_, err := c.Bucket.Upsert(fmt.Sprintf("doppler:client:%s", clientID), c.Doc.Events, 0)
+	c.Doc = &Doc{}
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ConnectToCB connects to couchbase and sets up the client's bucket.
+// conn is a connection string which be of the form couchbase://username:password@localhost/bucket_name.
+// returns an error which will be nil if no error has occured.
 func (c *Couchbase) ConnectToCB(conn string) error {
 	// parse the connection string into a url for later use
 	u, err := url.Parse(conn)
@@ -66,6 +106,8 @@ func (c *Couchbase) ConnectToCB(conn string) error {
 	if u.Path == "" || u.Path == "/" {
 		return errors.New("Bucket not specified")
 	}
+	c.bucketName = u.Path[1:]
+
 	// get the proper connection format (couchbase//host) and connect to the cluster
 	spec := fmt.Sprintf("%s://%s", u.Scheme, u.Host)
 	cluster, err := gocb.Connect(spec)
@@ -74,7 +116,7 @@ func (c *Couchbase) ConnectToCB(conn string) error {
 	}
 	// authenticate the user and connect to the specified bucket
 	cluster.Authenticate(&gocb.PasswordAuthenticator{Username: username, Password: password})
-	c.Bucket, err = cluster.OpenBucket(u.Path[1:], "")
+	c.Bucket, err = cluster.OpenBucket(c.bucketName, "")
 	if err != nil {
 		return err
 	}
