950ff89773fd3efc011e217aa53342d82cfe253a
deepmicrobe <leanderstevano@gmail.com>
2018-06-20 11:55:09 -0400

    Connected Kafka to Frontend via Websocket

--- cmd/doppler-api/main.go
+++ cmd/doppler-api/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/acstech/doppler-api/internal/consumer"
+	"github.com/acstech/doppler-api/internal/liveupdate"
 )
 
 func main() {
-
+	go liveupdate.StartWebsocket()
+	liveupdate.Consume()
 }
