d0b397c5d0d5ce9eeae00cd96d7a8e38b677d2ce
ben wornom <bworn621@students.bju.edu>
2018-06-21 11:01:37 -0400

    doing good

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ An API to consume data from Kafka and send it to the frontend via websockets.
 - [Look at general setup steps for Doppler-Events and ensure the same services are running](https://github.com/acstech/doppler-events#Setup)
 ### Couchbase
 - [Look here for Couchbase setup](https://github.com/acstech/doppler-events#couchbase)
-- Then, copy and rename the .env.defualt to .env and fill out the appropriate envirnment variable to connect to couchbase.
+- Then, copy and rename the .env.defualt in the root directory to .env and fill out the appropriate environment variables to connect to Couchbase. (Use "localhost" for "host" in development)
--- cmd/doppler-api/main.go
+++ cmd/doppler-api/main.go
@@ -1,10 +1,65 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	cb "github.com/acstech/doppler-api/internal/couchbase"
+	fx "github.com/acstech/doppler-api/internal/influx"
+	client "github.com/influxdata/influxdb/client/v2"
+	"github.com/joho/godotenv"
+
 	"github.com/acstech/doppler-api/internal/liveupdate"
 )
 
 func main() {
+
 	go liveupdate.StartWebsocket()
 	liveupdate.Consume()
+
+	//get CB config values from .env file
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		panic(err)
+	}
+	cbCon := os.Getenv("COUCHBASE_CONN")
+
+	//create CB connection
+	cbConn := &cb.Couchbase{Doc: &cb.Doc{}}
+	cbConn.ConnectToCB(cbCon)
+	fmt.Println("Created the db connection.")
+	if !cbConn.ClientExists("client0") { //client0 is hard coded client ID
+		fmt.Print("error")
+	}
+	//ensure that the eventID exists
+	//cbConn.EventEnsure("test2", "")
+
+	// creates influx client
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "http://localhost:8086",
+		Username: "root",
+		Password: "root",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer c.Close()
+
+	// queries influx for all points, query is temporarily hard coded
+	influx := false
+	if influx == true {
+		res, err := fx.GetPoints(c, "select * from dopplerDataHistory")
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			i := 0
+			for i < len(res.ValArray) {
+				fmt.Println(res.ValArray[i])
+				i = i + 1
+			}
+		}
+	}
+
+	fmt.Println("Client exists and the eventID has been ensured.")
 }
