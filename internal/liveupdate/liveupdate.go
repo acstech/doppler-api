56e3e98180c1b3b39c481ea236c39a1ebf99efb4
deepmicrobe <leander.stevano15@stjohns.edu>
2018-06-22 13:55:49 -0400

    websocket message reading basics (#27)

--- internal/liveupdate/liveupdate.go
+++ internal/liveupdate/liveupdate.go
@@ -6,38 +6,53 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/Shopify/sarama"
+	"github.com/acstech/doppler-api/internal/couchbase"
 	"github.com/gorilla/websocket"
 )
 
+// Force origin policy to be true in order to avoid 403 error
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-var Websockets map[string]ConnectionStruct
+// Manage clients, connections, filters of those connections
+var Websockets = make(map[string]map[*Connection]struct{})
 var count int = 0
+var cbConn *couchbase.Couchbase
 
-type ConnectionStruct struct {
+// Connection has a websocket connection and a filter setting
+type Connection struct {
 	addr          *websocket.Conn
 	filterSetting string
 }
 
+var connection Connection
+
+// Data to be sent to front end so it can be mapped
 type Point struct {
-	Latitude  string `json:"lat"`
-	Longitude string `json:"lng"`
-	Count     string `json:"count"`
-	Client    string `json:"clientID"`
-	Event     string `json:"eventID"`
+	Latitude  string `json:"lat,omitempty"`
+	Longitude string `json:"lng,omitempty"`
+	// Number of times this event has happened
+	Count  string `json:"count,omitempty"`
+	Client string `json:"clientID,omitempty"`
+	Event  string `json:"eventID,omitempty"`
 	// Insert time eventually
 }
 
 // Start a websocket connection for client
-func StartWebsocket() {
-	http.HandleFunc("/recieve/ws", dot)
-	Websockets = make(map[string]ConnectionStruct)
+// cbCon is the couchbase connection string
+func StartWebsocket(cbCon string) {
+	//create CB connection
+	cbConn = &couchbase.Couchbase{Doc: &couchbase.Doc{}}
+	// Connect to couchbase
+	cbConn.ConnectToCB(cbCon)
+	http.HandleFunc("/v3/ws", dot)
+	Websockets = make(map[string]map[*Connection]struct{})
 	fmt.Println("websocket running!")
 	http.ListenAndServe(":8000", nil)
 }
@@ -50,33 +65,55 @@ func dot(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
+
 	// Function to read any messages that are received
 	go func(conn *websocket.Conn) {
 		for {
-			_, _, err := conn.ReadMessage()
+			_, msg, err := conn.ReadMessage()
 			if err != nil {
 				conn.Close()
 				return
+			}
+
+			if strings.Contains(string(msg), "clientID") {
+				var point Point
+				// Unmarshal read message into point object
+				err := json.Unmarshal(msg, &point)
+				if err != nil {
+					panic(err)
+				}
+				// See if client id exists by running couchbase query
+				if cbConn.ClientExists(point.Client) {
+					// Pass client id as a key value
+					Websockets[point.Client] = make(map[*Connection]struct{})
+					err = conn.WriteJSON(cbConn.Doc.Events)
+					if err != nil {
+						panic(err)
+					}
+					fmt.Println("connected")
+				}
+			} else {
+				conn.WriteMessage(1, msg)
 			}
 		}
 	}(conn)
 
 	// Websockets[strconv.Itoa(count)] = conn
-	count++
-	fmt.Println(count)
+	// count++
+	// fmt.Println(count)
 }
 
 // Send points to front end
-func SendPoint(point Point, cs *websocket.Conn) {
-	for _, conn := range Websockets {
-		//conn.WriteJSON(point)
-		conn.addr.WriteJSON(Point{
-			Longitude: point.Longitude,
-			Latitude:  point.Latitude,
-			Count:     "3",
-		})
-	}
-}
+// func SendPoint(point Point, connection Connection) {
+// 	for _, conn := range Websockets {
+// 		//conn.WriteJSON(point)
+// 		connection.addr.WriteJSON(Point{
+// 			Longitude: point.Longitude,
+// 			Latitude:  point.Latitude,
+// 			Count:     "3",
+// 		})
+// 	}
+// }
 
 // Consume messages from queue
 func Consume() {
@@ -129,19 +166,19 @@ func Consume() {
 
 				//fmt.Println("Lat: " + point.Latitude + " Lng: " + point.Longitude)
 				// Check if ClientID exists
-				if _, contains := Websockets[point.Client]; contains {
-					// If it does create an instance of ConnectionStruct
-					cs := Websockets[point.Client]
+				// if _, contains := Websockets[point.Client]; contains {
+				// 	// If it does create an instance of ConnectionStruct
+				// 	cs := Websockets[point.Client]
 
-					if cs.filterSetting == "all" {
-						point.Count = "3"
-						SendPoint(point, cs.addr)
-						// Match filter setting to point event
-					} else if cs.filterSetting == point.Event {
-						point.Count = "3"
-						SendPoint(point, cs.addr)
-					}
-				}
+				// 	if cs.filterSetting == "all" {
+				// 		point.Count = "3"
+				// 		SendPoint(point, cs.addr)
+				// 		// Match filter setting to point event
+				// 	} else if cs.filterSetting == point.Event {
+				// 		point.Count = "3"
+				// 		SendPoint(point, cs.addr)
+				// 	}
+				// }
 			// Service interruption
 			case <-signals:
 				fmt.Println("Interrupt detected")
