3692c772e892b2c96160639c9abded9c7269f7c8
Matt Smith <38513513+mattsmith803@users.noreply.github.com>
2018-06-27 14:54:19 -0400

    refactored functions added comments (#32)

--- internal/liveupdate/liveupdate.go
+++ internal/liveupdate/liveupdate.go
@@ -23,6 +23,7 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+//GLOBAL VARIABLES
 var cbConn *couchbase.Couchbase                                   //used to hold couchbase connection
 var clientConnections map[string]map[*ConnWithParameters]struct{} //map used as connection hub, keeps up with clients and their respective connections and each connections settings
 var mutex = &sync.RWMutex{}                                       //mutex used for concurrent reading and writing
@@ -31,7 +32,7 @@ var maxBatchSize = 50                                             //max size of 
 var minBatchSize = 1                                              //min size of data batch that is sent
 var batchInterval = time.Duration(1000 * time.Millisecond)        //millisecond interval that data is sent
 
-//used to add parameters to a gorilla's websocket.Conn
+//ConnWithParameters is used to add parameters to a gorilla's websocket.Conn
 type ConnWithParameters struct {
 	ws         *websocket.Conn     //keeps up with connection identifier
 	clientID   string              //the clientID associated with this connection
@@ -40,11 +41,12 @@ type ConnWithParameters struct {
 	batchArray []KafkaData         //array used to hold data for batch sending
 }
 
+//BatchStruct is the JSON format for batch
 type BatchStruct struct {
 	BatchArray []KafkaData `json:"batchArray"`
 }
 
-//JSON format messages from client
+//msg is the JSON format messages from client
 type msg struct {
 	ClientID string   `json:"clientID,omitempty"`
 	Filter   []string `json:"Filter,omitempty"`
@@ -52,17 +54,16 @@ type msg struct {
 	//endTime <type> `json:"endTime, omitempty"`
 }
 
-//JSON format messages from Kafka
+//KafkaData is the JSON format messages from Kafka
 type KafkaData struct {
 	Latitude  string `json:"lat,omitempty"`
 	Longitude string `json:"lng,omitempty"`
 	Count     string `json:"count,omitempty"`
 	ClientID  string `json:"clientID,omitempty"`
 	EventID   string `json:"eventID,omitempty"`
-	// Insert time eventually
 }
 
-// Initialize listening for websocket requests
+//InitWebsockets initializes websocket requests
 func InitWebsockets(cbConnection string) {
 	//create CB connection
 	cbConn = &couchbase.Couchbase{Doc: &couchbase.Doc{}}
@@ -108,30 +109,24 @@ func createWS(w http.ResponseWriter, r *http.Request) {
 	go readWS(conn)
 }
 
+//readWS continually reads messages from a ConnWithParameters' websocket, initializes connection parameters and updates live filters when necessary
 func readWS(conn *ConnWithParameters) {
-	defer conn.ws.Close()
+	defer conn.ws.Close() //close the connection whenever readWS returns
 
 	//boolean used to keep up if this websocket has been connected
 	connected := false
 
-	// Function to read any messages that are received
+	// Continuously read messages that are received
 	for {
 		//read messages from client
 		_, msgBytes, err := conn.ws.ReadMessage()
 		//check if client closed connection
 		if err != nil {
-			fmt.Println("Connection Closed by Client")
-			//REMOVE FROM MAP
-			mutex.Lock()
-			delete(clientConnections[conn.clientID], conn)
-			//check if client has any remaining connections, if so, delete client
-			if len(clientConnections[conn.clientID]) == 0 {
-				delete(clientConnections, conn.clientID)
-			}
-			fmt.Println("Removed Conn: ", clientConnections)
-			mutex.Unlock()
-			return
+			//if client closed connection, remove connetion from clientConnections
+			closeConnection(conn)
+			return //returns out of readWS
 		}
+
 		//declare message that will hold client message data
 		var message msg
 		//unmarshal (convert bytes to msg struct)
@@ -141,86 +136,21 @@ func readWS(conn *ConnWithParameters) {
 		}
 
 		//WEBSOCKET MANAGEMENT
-		//Initialize all websocket parameters
+		//If havent been connected, initialize all connection parameters, first message has to be clientID
 		if !connected {
-			//update conn with new parameters
-			//add clientID to connection
-			conn.clientID = message.ClientID
-
-			//check if client is already connected on another websocket
-			//if client has not been connected, create new connection map
-			mutex.Lock()
-			if _, contains := clientConnections[message.ClientID]; !contains {
-				clientConnections[message.ClientID] = make(map[*ConnWithParameters]struct{})
-			}
-			mutex.Unlock()
-			//add conn to map
-			mutex.Lock()
-			clientConnections[message.ClientID][conn] = struct{}{}
-			fmt.Println("Added Conn", clientConnections)
-			mutex.Unlock()
-			fmt.Println()
+			conn = initConn(conn, message)
 			//update connected to true
 			connected = true
-
-			//CHECK COUCHBASE
-			//check if client exists in couchbase
-			if cbConn.ClientExists(message.ClientID) {
-				//query couchbase for client's events
-				clientEvents := cbConn.Doc.Events
-
-				//add filters to connection
-				for _, event := range clientEvents {
-					//add live filters
-					conn.filter[event] = struct{}{}
-					// Add event to allfilters map
-					conn.allFilters[event] = struct{}{}
-				}
-				//send event options to client
-				conn.ws.WriteJSON(clientEvents)
-			} else {
-				//if clientID does not exist in couchbase
-				conn.ws.WriteMessage(1, []byte("Couchbase Error: ClientID not found"))
-			}
-			//start writing Function
-			go intervalFlush(conn)
 			//continue to next for loop iteration, skipping updating filters
 			continue
 		}
 
-		//UPDATE FILTERS
-		conn.filter = make(map[string]struct{}) //reinitialize filters because receiving whole array of filters from client
-		//iterate through client message filter array and add the elements to the connection filter slice
-		for _, event := range message.Filter {
-			conn.filter[event] = struct{}{}
-		}
-	}
-}
-func intervalFlush(conn *ConnWithParameters) {
-	var flushTime time.Time
-	for {
-		if time.Now().Sub(flushTime) >= batchInterval {
-			if len(conn.batchArray) > minBatchSize {
-				// fmt.Println("Interval Flush")
-				mutex.Lock()
-				flush(conn)
-				mutex.Unlock()
-				flushTime = time.Now()
-			}
-		}
+		//UPDATE LIVE FILTERS - if client has already been connected, only other messages should be filter updates
+		conn = updateLiveFilters(conn, message)
 	}
 }
 
-func flush(conn *ConnWithParameters) {
-	batch, err := json.Marshal(conn.batchArray)
-	if err != nil {
-		fmt.Println("batch marshal error")
-	}
-	conn.ws.WriteJSON(string(batch))
-	conn.batchArray = []KafkaData{}
-}
-
-// Consume messages from queue
+// Consume consumes messages from queue
 func Consume() {
 	fmt.Println("Kafka Consume Started")
 	// Create a new configuration instance
@@ -237,8 +167,8 @@ func Consume() {
 
 	// Wait to close after everything is processed
 	defer func() {
-		if err := master.Close(); err != nil {
-			panic(err)
+		if closeErr := master.Close(); err != nil {
+			panic(closeErr)
 		}
 	}()
 
@@ -258,6 +188,7 @@ func Consume() {
 
 	// Signal to finish
 	doneCh := make(chan struct{})
+	//go func that continually consumes messages from Kafka
 	go func() {
 	Loop:
 		for {
@@ -267,9 +198,9 @@ func Consume() {
 				fmt.Println(err)
 			// Print consumer messages
 			case msg := <-consumer.Messages():
-				// fmt.Println("Got data ", string(msg.Value))
+				//initialize variable to hold data from kafka data
 				var kafkaData KafkaData
-				json.Unmarshal(msg.Value, &kafkaData)
+				json.Unmarshal(msg.Value, &kafkaData) //unmarshal data to json
 
 				// Check if ClientID exists
 				mutex.Lock()
@@ -280,7 +211,7 @@ func Consume() {
 					for conn := range clientConnections {
 						// Check if consume message has a different filter than allfilters
 						if _, contains := conn.allFilters[kafkaData.EventID]; !contains {
-							updateFilter(conn, kafkaData.EventID)
+							updateAvailableFilters(conn, kafkaData.EventID)
 						}
 
 						//if connection filter has KafkaData eventID, send data
@@ -317,14 +248,120 @@ func Consume() {
 	fmt.Println("Consumption closed")
 }
 
-// Update filter once there is a change
-func updateFilter(conn *ConnWithParameters, newFilter string) {
+//initConn initialize a ConnWithParameters' parameters based on the first message sent over the websocket
+func initConn(conn *ConnWithParameters, message msg) *ConnWithParameters {
+	//update conn with new parameters
+	//add clientID to connection
+	conn.clientID = message.ClientID
+
+	//check if client is already connected on another websocket
+	//if client has not been connected, create new connection map
+	mutex.Lock()
+	if _, contains := clientConnections[message.ClientID]; !contains {
+		clientConnections[message.ClientID] = make(map[*ConnWithParameters]struct{})
+	}
+	mutex.Unlock()
+
+	//add conn to clients' map of connections
+	mutex.Lock()
+	clientConnections[message.ClientID][conn] = struct{}{}
+	fmt.Println("Added Conn", clientConnections)
+	mutex.Unlock()
+	fmt.Println()
+
+	//CHECK COUCHBASE for client's data
+	//check if client exists in couchbase
+	if cbConn.ClientExists(message.ClientID) {
+		//query couchbase for client's events
+		clientEvents := cbConn.Doc.Events
+
+		//add filters to connection
+		for _, event := range clientEvents {
+			//add live filters (because defaulting initialize live filters to all)
+			conn.filter[event] = struct{}{}
+			// Add event to allfilters map
+			conn.allFilters[event] = struct{}{}
+		}
+		//send event options to client
+		conn.ws.WriteJSON(clientEvents)
+	} else {
+		//if clientID does not exist in couchbase
+		conn.ws.WriteMessage(1, []byte("Couchbase Error: ClientID not found"))
+	}
+	//start checking if need to flush batch
+	go intervalFlush(conn)
+
+	return conn
+}
+
+//closeConnection removes the connection from the client, if the client has no connections, removes the client
+func closeConnection(conn *ConnWithParameters) {
+	fmt.Println("Connection Closed by Client")
+	//REMOVE FROM MAP
+	mutex.Lock()
+	delete(clientConnections[conn.clientID], conn) //delete specific connection
+	//check if client has any remaining connections, if so, delete client
+	if len(clientConnections[conn.clientID]) == 0 {
+		delete(clientConnections, conn.clientID)
+	}
+	fmt.Println("Removed Conn: ", clientConnections)
+	mutex.Unlock()
+}
+
+//intervalFlush determines when to flush the batch based on the time of the last flush
+func intervalFlush(conn *ConnWithParameters) {
+	//initialize time of flush
+	var flushTime time.Time
+	//continuously check if need to flush because of time interval
+	for {
+		//see if current time minus last flush time is greater than or equal to the set interval
+		//sub returns type Duration, batchInterval is of type Duration
+		if time.Now().Sub(flushTime) >= batchInterval {
+			if len(conn.batchArray) > minBatchSize {
+				// fmt.Println("Interval Flush")
+				mutex.Lock()
+				flush(conn)
+				mutex.Unlock()
+				flushTime = time.Now()
+			}
+		}
+	}
+}
+
+//flush marshals the batch to json, sends the batch over the conn's websocket, and emptys the batch
+func flush(conn *ConnWithParameters) {
+	batch, marshalErr := json.Marshal(conn.batchArray) //marshal to type BatchStruct
+	if marshalErr != nil {
+		fmt.Println("batch marshal error")
+	}
+	writeErr := conn.ws.WriteJSON(string(batch)) //send batch to client
+	if writeErr != nil {
+		fmt.Println(writeErr)
+	}
+	conn.batchArray = []KafkaData{} //empty batch
+}
+
+//updateLiveFilters removes the current filters and sets filter equal to the new filters found in the message
+func updateLiveFilters(conn *ConnWithParameters, message msg) *ConnWithParameters {
+	conn.filter = make(map[string]struct{}) //empty current filters
+	//iterate through client message filter array and add the elements to the connection filter slice
+	for _, event := range message.Filter {
+		conn.filter[event] = struct{}{} //add the new filters
+	}
+	return conn
+}
+
+// updateAvailableFilters adds a new filter found in consume messages to allFilters and sends the available filters to the client
+func updateAvailableFilters(conn *ConnWithParameters, newFilter string) {
 	// Add new filter to map
 	conn.allFilters[newFilter] = struct{}{}
+	//initilize slice for sending to client
 	var clientEvents []string
-	for key, _ := range conn.allFilters {
+	//add all of the available filters to the slice
+	for key := range conn.allFilters {
 		clientEvents = append(clientEvents, key)
 	}
+	//send slice to client
 	err := conn.ws.WriteJSON(clientEvents)
 	if err != nil {
 		fmt.Println(err)
