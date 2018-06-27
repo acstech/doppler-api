b27328d5425916ed9c33648512e3173b08fdab4b
bwornom7 <bworn621@students.bju.edu>
2018-06-27 17:27:18 -0400

    Ben/reorganize (#34)
    * working towards sending batched points up to webpage
    
    * handle connection requests containing bad clientIDs

--- internal/influx/influxclient.go
+++ internal/influx/influxclient.go
@@ -22,26 +22,7 @@ type Responses struct {
 	ValArray []InfluxResponse
 }
 
-/*
-func GetPoints(clnt client.Client, query string) (res []client.Result, err error) {
-	q := client.NewQuery(string, DB, "ms")
-  if response, err := clnt.Query(q); err == nil && response.Error() == nil {
-		fmt.Println(response.Results)
-	}
-	return res, nil
-}*/
-
 func GetPoints(clnt client.Client, query string) (res Responses, err error) {
-	//var clnt client.Client
-	//Consume()
-	/*cbConn := &cb.Couchbase{Doc: &cb.Doc{}}
-	cbConn.ConnectToCB("couchbase://validator:rotadilav@localhost/doppler1")
-	fmt.Println("Created the db connection.")
-	if !cbConn.ClientExists("test2") {
-		fmt.Print("error")
-	}
-	//ensure that the eventID exists
-	//cbConn.EventEnsure("test2", "")*/
 
 	q := client.NewQuery(query, DB, "s")
 	if response, err := clnt.Query(q); err == nil && response.Error() == nil {
