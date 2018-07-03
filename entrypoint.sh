8b9ff0a619f8068ded56b3a351ba588a271126ff
pjkaufman <PeterJamesKaufman@gmail.com>
2018-07-03 11:05:16 -0400

    Updated environment variables and added docker files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 echo "Making sure kafka and couchbase are up"
+<<<<<<< HEAD
 while ! nc -z couchbase 8091 || ! nc -z kafka 9092 ;
+=======
+while [ ! nc -z kafka 9092 ] && [ ! nc -z couchbase 8091 ];
+>>>>>>> Updated environment variables and added docker files
 do
     echo sleeping;
     sleep 1;
 done;
+<<<<<<< HEAD
 echo "Kafka and Couchbase are up"
+=======
+>>>>>>> Updated environment variables and added docker files
 # safe to run the service
 /opt/service/frontendAPI
     
