e7e15fa472a64534dd46af3ffc034db7c76f6afa
pjkaufman <PeterJamesKaufman@gmail.com>
2018-07-05 09:53:16 -0400

    updated docker and readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 echo "Making sure kafka and couchbase are up"
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 while ! nc -z couchbase 8091 || ! nc -z kafka 9092 ;
 =======
 while [ ! nc -z kafka 9092 ] && [ ! nc -z couchbase 8091 ];
@@ -9,10 +10,14 @@ while [ ! nc -z kafka 9092 ] && [ ! nc -z couchbase 8091 ];
 =======
 while [ ! nc -z kafka 9092 ] && [ ! nc -z couchbase 8091 ];
 >>>>>>> Updated environment variables and added docker files
+=======
+while ! nc -z couchbase 8091 || ! nc -z kafka 9092 ;
+>>>>>>> updated docker and readme
 do
     echo sleeping;
     sleep 1;
 done;
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 echo "Kafka and Couchbase are up"
@@ -20,6 +25,9 @@ echo "Kafka and Couchbase are up"
 >>>>>>> Updated environment variables and added docker files
 =======
 >>>>>>> Updated environment variables and added docker files
+=======
+echo "Kafka and Couchbase are up"
+>>>>>>> updated docker and readme
 # safe to run the service
 /opt/service/frontendAPI
     
